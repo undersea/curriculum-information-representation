1174d698e3cc1499fb542dec9e5878673579b8ee
jdhurfor <jdhurfor@93a84c75-b7d8-402c-aa9c-cfdb53399b0d>
2010-07-14 04:46:07 +0000

    Adding in som from MLcode.zip which is Stephens python code for SOM
    git-svn-id: https://ist-linux1.massey.ac.nz/svn/jdhurfor/trunk/curriculum-information-representation@365 93a84c75-b7d8-402c-aa9c-cfdb53399b0d

--- src/graph/edge.py
+++ src/graph/edge.py
@@ -4,5 +4,8 @@ from node import Node
 
 class Edge(Node):
     def __init__(self):
-        pass
+        self.vertex1 = None
+        self.vertex2 = None
 
+    def get_vertices(self):
+        return [self.vertex1, self.vertex2]
