f7ca385faea144966a71ac76951a2c46fa9269bb
jdhurfor <jdhurfor@93a84c75-b7d8-402c-aa9c-cfdb53399b0d>
2010-07-19 06:44:55 +0000

    Added test for SOM
    Made sure that som/som.py can find unsupervised/som.py and made sure unsupervised has the __init__.py file to turn it into a package.  Thus SOM can inherit from som and the test can be run succesfully.  It still needs unsupervised.som.som to inherit from object otherwise the super call in SOM will not work due to it being a classic class style and not the new style classes which are required.
    
    git-svn-id: https://ist-linux1.massey.ac.nz/svn/jdhurfor/trunk/curriculum-information-representation@383 93a84c75-b7d8-402c-aa9c-cfdb53399b0d

--- src/som/som.py
+++ src/som/som.py
@@ -1,6 +1,11 @@
 
-from unsupervised import som
+from unsupervised.som import som
 
 class SOM(som):
-    def __init__(self):
-        pass
+    def __init__(self, *args, **kwargs):
+        try:
+            #super class 'som' must be a newstyle class inheriting from object
+            #for the following super() statment to work
+            super(SOM, self).__init__(*args, **kwargs)
+        except:
+            som.__init__(self, *args, **kwargs)
