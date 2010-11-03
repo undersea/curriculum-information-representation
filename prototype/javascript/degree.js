4cb070599af20fc3da7c770ec7e528cf5676fcc4
James Hurford <terrasea@gmail.com>
2010-11-03 17:26:49 +1300

    Done some stuff with raphael and javascript regular expressions
    Figured out how to create raphael objects and interact with them
    through click events, and animations.
    
    Found can do regular expressions in javascript

---
+++ prototype/javascript/degree.js
@@ -0,0 +1,24 @@
+var degree = {
+  binfsci: [['158\d{3}', '159\d{3}', '160\d{3}']{'comp sci':['159101','159102']}]
+};
+
+var initDegree = function  (degree,container,width,height) {
+  var paper = Raphael(container, width, height);
+  
+};
+
+
+var getRequiredPapers =function(degree) {
+  return [];
+};
+
+
+
+var getPrerequisites = function(paper) {
+  return [];
+};
+
+
+var getNextPossible = function (paper) {
+  return [];
+};
