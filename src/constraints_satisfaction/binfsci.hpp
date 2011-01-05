04b0edfa1102593e707f053be2367daa3bb4ff44
James Hurford <terrasea@gmail.com>
2011-01-06 08:19:25 +1300

    Started thinking of how I can approach the COP with degrees

---
+++ src/constraints_satisfaction/binfsci.hpp
@@ -0,0 +1,17 @@
+#ifndef _BINFSCI_HPP_
+#define _BINFSCI_HPP_
+
+#include <gecode/int.hh>
+#include <gecode/minimodel.hh>
+#include <gecode/search.hh>
+
+using namespace Gecode;
+
+#include "rules.hpp"
+
+class BInfSci : public Rules
+{
+
+};
+
+#endif // _BINFSCI_HPP_
---
+++ src/constraints_satisfaction/rules.hpp
@@ -0,0 +1,10 @@
+#ifndef _RULES_HPP_
+#define _RULES_HPP_
+
+class Rules
+{
+public:
+  
+};
+
+#endif // _RULES_HPP_
