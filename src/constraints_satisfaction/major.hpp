b8a1cf63b36fcf34fe6ea3ff5190f7ac0b34a19c
James Hurford <terrasea@gmail.com>
2011-01-22 12:15:37 +1300

    More work on setting up major filter.

--- src/constraints_satisfaction/major.hpp
+++ src/constraints_satisfaction/major.hpp
@@ -1,6 +1,8 @@
 #ifndef __MAJOR_HPP__
 #define __MAJOR_HPP__
 
+#include <gecode/int.hh>
+#include <gecode/minimodel.hh>
 
 typedef MinimizeSpace MaximiseSpace
 
@@ -10,11 +12,14 @@ namespace Programme
   class Major : public MaximiseSpace
   {
   public:
-
+    Major(void);
   protected:
 
   private:
     BoolVarArray majors;
+    std::map<string, int> names_to_pos;
+    std::map<int, string> pos_to_names;
+    std::map<int, std::vector<int>> major_papers;
   };
 
 
