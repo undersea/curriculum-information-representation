0971e7b9351b8ce4825fa8bdaf0c181a14fc5ff0
James Hurford <terrasea@gmail.com>
2011-01-21 17:00:36 +1300

    Starting again and adding a log entry explaining why, wrote more report

---
+++ src/constraints_satisfaction/major.hpp
@@ -0,0 +1,23 @@
+#ifndef __MAJOR_HPP__
+#define __MAJOR_HPP__
+
+
+typedef MinimizeSpace MaximiseSpace
+
+namespace Programme
+{
+  
+  class Major : public MaximiseSpace
+  {
+  public:
+
+  protected:
+
+  private:
+    BoolVarArray majors;
+  };
+
+
+} // Programme
+
+#endif // __MAJOR_HPP__
