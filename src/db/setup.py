fa20384fd3da77e92cc7ef65e2e69de307e6a358
James Hurford <terrasea@gmail.com>
2011-01-27 09:01:14 +1300

    added sql statements to setup database

--- src/db/setup.py
+++ src/db/setup.py
@@ -11,3 +11,13 @@ row = cursor.fetchone()
 while row != None:
     print row
     row = cursor.fetchone()
+"""
+create table paper_prereq
+(
+  id integer not null auto_increment,
+  code integer not null,
+  combo_id integer,
+  prereq integer
+);
+GRANT ALL PRIVILEGES ON programme_planner.* TO workload @'%';
+"""
