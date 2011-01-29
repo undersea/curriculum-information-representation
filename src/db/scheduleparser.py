a6144f598a3a8f6c34f3fb4ccf09482d746e837c
James Hurford <terrasea@gmail.com>
2011-01-30 12:06:36 +1300

    scheduleparser.py transformed to python 3.0 using 2to3 programme

--- src/db/scheduleparser.py
+++ src/db/scheduleparser.py
@@ -1,8 +1,4 @@
-try:
-    from HTMLParser import HTMLParser
-except:
-    #must be python 3.x
-    from html.parser import HTMLParser
+from html.parser import HTMLParser
 
 
 class ScheduleExtracter(HTMLParser):
@@ -16,38 +12,49 @@ class ScheduleExtracter(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == 'h4':
             self.h4 = True
-            print 'Start tag: ', tag
+            
+            print('Start tag: ', tag)
+            
         else:
             if self.major and tag == 'table':
-                print 'start of major schedule table'
+                print('start of major schedule table')
                 self.table = True
             if self.major and self.table:
-                print '"<%s>"'% (tag),
+                print('"<%s>"'% (tag), end=' ')
+            
     
     def handle_endtag(self, tag):
         if tag == 'h4':
             self.h4 = False
-            print 'End tag: ', tag
+
+            print('End tag: ', tag)
+
+                
         else:
             if self.major and self.table:
-                print '"</%s>"'% (tag),
+                
+                print('"</%s>"'% (tag), end=' ')
+                
             if self.major and self.table and tag == 'table':
-                print 'end of table', tag
+                
+                print('end of table', tag)
+                
                 self.major = False
                 self.table = False
 
     def handle_data(self, data):
         if self.h4:
-            print "'%s' %s" % (data, len(data))
+            print("'%s' %s" % (data, len(data)))
             if str(data) == 'Agricultural Science':
                 self.major = True
             
 
 
 if __name__ == '__main__':
-    import urllib2
-    sock = urllib2.urlopen('http://www.massey.ac.nz/massey/about-massey/calendar/degree-diploma-and-certificate-regulations/college-of-sciences/en/bachelor-of-science.cfm')
+    import urllib.request, urllib.error, urllib.parse
+    sock = urllib.request.urlopen('http://www.massey.ac.nz/massey/about-massey/calendar/degree-diploma-and-certificate-regulations/college-of-sciences/en/bachelor-of-science.cfm')
     data = sock.read()
+    print(type(data))
     p = ScheduleExtracter()
-    p.feed(data)
+    p.feed(str(data))
 
