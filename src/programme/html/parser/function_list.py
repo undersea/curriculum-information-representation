7bdc495e98acb54d10174a1a242786974b18e03d
James Hurford <terrasea@gmail.com>
2011-01-31 04:29:38 +1300

    Now uses lxml xtree to parse and use xpath to retrieve major papers with

--- src/programme/html/parser/function_list.py
+++ src/programme/html/parser/function_list.py
@@ -1,5 +1,6 @@
 
 import libxml2 as xml
+from lxml import etree
 import page_finder as finder
 import parser
 import re
@@ -25,7 +26,25 @@ class AccountancyFunction(ProgrammeFunction):
 
 
 
-functions = {'':ProgrammeFunction(), 'Bachelor of Accountancy (BAcc)': AccountancyFunction(),'Bachelor of AgriScience (Agriculture)':ProgrammeFunction()}
+class BSc(ProgrammeFunction):
+    def __init__(self, major):
+        super(BSc, self).__init__()
+
+    def __call__(self):
+        f = StringIO(self.htmlstr)
+        tree = etree.parse(f)
+        tmp = tree.getroot().xpath('//xhtml:h4[text()="%s"]/following-sibling::xhtml:table[1]/xhtml:tbody/xhtml:tr/xhtml:td[1]/text()', namespaces={'xhtml':'http://www.w3.org/1999/xhtml' % (major)})
+        tmp = [x for x in tmp if len(x) > 1]
+        if len(tmp) == 0:
+            tmp = tree.getroot().xpath('//xhtml:h4[text()="Computer Science"]/../../..//xhtml:tr/xhtml:td[1]/text()', namespaces={'xhtml':'http://www.w3.org/1999/xhtml'})
+
+            if len(tmp) == 0:
+                return False
+
+        return set(tmp)
+
+
+functions = {'':ProgrammeFunction(), 'Bachelor of Accountancy (BAcc)': AccountancyFunction(),'Bachelor of AgriScience (Agriculture)':ProgrammeFunction(), 'Bachelor of Science (Animal Science)':BSc('Animal Science')}
 
 def get_function(programme):
     try:
